0b3e15195948e0e4986f18ca7656ed352aef922e
Teng-Ju Yang <tyang28@jhu.edu>
2019-04-06 19:01:25 -0400

    Made a registration page

--- app/src/main/java/com/example/uniporter_app/register.java
+++ app/src/main/java/com/example/uniporter_app/register.java
@@ -1,4 +1,13 @@
 package com.example.uniporter_app;
 
-public class register {
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register2);
+    }
 }

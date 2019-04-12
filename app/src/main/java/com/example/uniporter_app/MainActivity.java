fa4fbb3d778d4b153b9fdc391b7fad9b3e74e712
jxing8 <jxing8@jhu.edu>
2019-04-11 22:57:03 -0400

    Added RecyclerView, Attached NavDrawer

--- app/src/main/java/com/example/uniporter_app/MainActivity.java
+++ app/src/main/java/com/example/uniporter_app/MainActivity.java
@@ -3,8 +3,6 @@ package com.example.uniporter_app;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -12,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Intent intent = new Intent(this, Login.class);
+        Intent intent = new Intent(this, NewRide.class);
         startActivity(intent);
     }
 }

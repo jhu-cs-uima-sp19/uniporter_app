df72095f3e2adc74f291aa95f771f22b789aed1b
Lilian Zhou <lilianhyz@gmail.com>
2019-04-13 18:27:24 -0400

    changed add new ride

--- app/src/main/java/com/example/uniporter_app/AddNewRide.java
+++ app/src/main/java/com/example/uniporter_app/AddNewRide.java
@@ -1,15 +1,10 @@
 package com.example.uniporter_app;
-import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.widget.Spinner;
 public class AddNewRide extends AppCompatActivity {
 
     //front buttons
@@ -32,77 +27,29 @@ public class AddNewRide extends AppCompatActivity {
         setContentView(R.layout.activity_flight_info);
 
         // bind view elements
-        _front1 = (ImageButton) findViewById(R.id.front1);
-        _front2 = (ImageButton) findViewById(R.id.front2);
-        _front3 = (ImageButton) findViewById(R.id.front3);
-        _front4 = (ImageButton) findViewById(R.id.front4);
-        _front5 = (ImageButton) findViewById(R.id.front5);
+        _front1 = findViewById(R.id.front1);
+        _front2 = findViewById(R.id.front2);
+        _front3 = findViewById(R.id.front3);
+        _front4 = findViewById(R.id.front4);
+        _front5 = findViewById(R.id.front5);
 
-        _back2 = (ImageButton) findViewById(R.id.back2);
-        _back3 = (ImageButton) findViewById(R.id.back3);
-        _back4 = (ImageButton) findViewById(R.id.back4);
-        _back5 = (ImageButton) findViewById(R.id.back5);
+        _back2 = findViewById(R.id.back2);
+        _back3 = findViewById(R.id.back3);
+        _back4 = findViewById(R.id.back4);
+        _back5 = findViewById(R.id.back5);
 
 
         //front buttons
-        @Override
-                _front1.setOnClickListener(new View.OnClickListener() {
+        _front1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
             Fragment fragment = new FlightInfo();
-             if (fragment != null) {
-                FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-                ft.replace(R.id.screen_area, fragment);
-                ft.commit();
-            }
-        });
-        _front2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                setContentView(R.layout.activity_blocks);
-            }
-        });
-        _front3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                setContentView(R.layout.activity_luggage);
-            }
-        });
-        _front4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                setContentView(R.layout.activity_early);
-            }
-        });
-        _front5.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                setContentView(R.layout.activity_confirmation);
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.replace(R.id.screen_area, fragment);
+            ft.commit();
+
             }
         });
 
-        //back buttons
-        _back2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                setContentView(R.layout.activity_flight_info);
-            }
-        });
-        _back3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                setContentView(R.layout.activity_address);
-            }
-        });
-        _back4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                setContentView(R.layout.activity_blocks);
-            }
-        });
-        _back5.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                setContentView(R.layout.activity_luggage);
-            }
-        });
     }
 }

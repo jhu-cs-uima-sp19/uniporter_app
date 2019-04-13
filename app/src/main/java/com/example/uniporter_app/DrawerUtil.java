fa68bafce8542aaf769722e87739783f3ae04ee8
Teng-Ju Yang <tyang28@jhu.edu>
2019-04-13 17:19:20 -0400

    added elements to drawer

--- app/src/main/java/com/example/uniporter_app/DrawerUtil.java
+++ app/src/main/java/com/example/uniporter_app/DrawerUtil.java
@@ -25,13 +25,11 @@ public class DrawerUtil {
         PrimaryDrawerItem drawerItemManagePlayers = new PrimaryDrawerItem().withIdentifier(1)
                 .withName(R.string.new_ride).withIcon(R.drawable.ic_add_circle_outline_black_24dp);
         PrimaryDrawerItem drawerItemManagePlayersTournaments = new PrimaryDrawerItem()
-                .withIdentifier(2).withName(R.string.scheduled_rides).withIcon(R.drawable.ic_schedule_24dp);
+                .withIdentifier(2).withName(R.string.scheduled_rides).withIcon(R.drawable.ic__car_24dp);
         PrimaryDrawerItem notifications = new PrimaryDrawerItem()
                 .withIdentifier(3).withName(R.string.scheduled_rides).withIcon(R.drawable.ic_notifications_active_black_24dp);
-        PrimaryDrawerItem myrides = new PrimaryDrawerItem()
-                .withIdentifier(4).withName(R.string.myrides).withIcon(R.drawable.ic__car_24dp);
         PrimaryDrawerItem logout = new PrimaryDrawerItem()
-                .withIdentifier(5).withName(R.string.myrides).withIcon(R.drawable.ic_exit_to_app_black_24dp);
+                .withIdentifier(4).withName(R.string.myrides).withIcon(R.drawable.ic_exit_to_app_black_24dp);
 
 
 
@@ -60,6 +58,8 @@ public class DrawerUtil {
         drawerBuilder.addDrawerItems(
                 drawerItemManagePlayers,
                 drawerItemManagePlayersTournaments,
+                notifications,
+                logout,
                 new DividerDrawerItem()
         );
 

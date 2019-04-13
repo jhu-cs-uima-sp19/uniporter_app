591f29a683c39e853b26ee18ea2df394da7e3b32
Madhu Rajmohan <madhu@Madhus-MacBook-Pro.local>
2019-04-13 14:58:58 -0400

    image buttons changed

--- app/src/main/java/com/example/uniporter_app/AddNewRide.java
+++ app/src/main/java/com/example/uniporter_app/AddNewRide.java
@@ -9,6 +9,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -21,16 +22,16 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class AddNewRide extends AppCompatActivity {
 
-    Button _front1;
-    Button _front2;
-    Button _front3;
-    Button _front4;
-    Button _front5;
+    ImageButton _front1;
+    ImageButton _front2;
+    ImageButton _front3;
+    ImageButton _front4;
+    ImageButton _front5;
 
-    Button _back2;
-    Button _back3;
-    Button _back4;
-    Button _back5;
+    ImageButton _back2;
+    ImageButton _back3;
+    ImageButton _back4;
+    ImageButton _back5;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -38,16 +39,16 @@ public class AddNewRide extends AppCompatActivity {
         setContentView(R.layout.activity_flight_info);
 
         // bind view elements
-        _front1 = findViewById(R.id.front1);
-        _front2 = findViewById(R.id.front2);
-        _front3 = findViewById(R.id.front3);
-        _front4 = findViewById(R.id.front4);
-        _front5 = findViewById(R.id.front5);
+        _front1 = (ImageButton) findViewById(R.id.front1);
+        _front2 = (ImageButton) findViewById(R.id.front2);
+        _front3 = (ImageButton) findViewById(R.id.front3);
+        _front4 = (ImageButton) findViewById(R.id.front4);
+        _front5 = (ImageButton) findViewById(R.id.front5);
 
-        _back2 = findViewById(R.id.back2);
-        _back3 = findViewById(R.id.back3);
-        _back4 = findViewById(R.id.back4);
-        _back5 = findViewById(R.id.back5);
+        _back2 = (ImageButton) findViewById(R.id.back2);
+        _back3 = (ImageButton) findViewById(R.id.back3);
+        _back4 = (ImageButton) findViewById(R.id.back4);
+        _back5 = (ImageButton) findViewById(R.id.back5);
 
 
         _front1.setOnClickListener(new View.OnClickListener() {

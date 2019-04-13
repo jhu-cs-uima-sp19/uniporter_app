6342e79884d698f618d6b57750aa67d3a6227888
Lilian Zhou <lilianhyz@gmail.com>
2019-04-13 08:41:02 -0400

    added start page again and connected it

---
+++ app/src/main/java/com/example/uniporter_app/Start.java
@@ -0,0 +1,42 @@
+package com.example.uniporter_app;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class Start extends AppCompatActivity {
+    Button _loginButton;
+    Button _registerButton;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.start_page);
+
+        // bind view elements
+        _loginButton = findViewById(R.id.firstlogin_button);
+        _registerButton = findViewById(R.id.firstreg_button);
+        _loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Login.class);
+                startActivity(intent);
+                finish();
+                overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
+            }
+        });
+        _registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Register.class);
+                startActivity(intent);
+                finish();
+                overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
+            }
+        });
+
+    }
+
+}

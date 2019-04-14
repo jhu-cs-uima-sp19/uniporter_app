6b58bd1097373318d732d64cf3206d9ddc9e72cd
Lilian Zhou <lilianhyz@gmail.com>
2019-04-14 12:36:54 -0400

    forward navigation

--- app/src/main/java/com/example/uniporter_app/Early.java
+++ app/src/main/java/com/example/uniporter_app/Early.java
@@ -1,16 +1,33 @@
 package com.example.uniporter_app;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 import android.view.View;
+import android.widget.ImageButton;
 
-public class Early extends Fragment {
-
+public class Early extends Fragment implements View.OnClickListener {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.activity_early, container, false);
+        View myView = inflater.inflate(R.layout.activity_early, container, false);
+        ImageButton front5 = myView.findViewById(R.id.front5);
+        front5.setOnClickListener(this);
+        return myView;
+    }
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+    @Override
+    public void onClick(View v) {
+        Fragment fragment = new Confirmation();
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+        ft.replace(R.id.screen_area, fragment, "Confirmation");
+        ft.commit();
     }
 }
--- app/src/main/java/com/example/uniporter_app/Luggage.java
+++ app/src/main/java/com/example/uniporter_app/Luggage.java
@@ -1,16 +1,33 @@
 package com.example.uniporter_app;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 import android.view.View;
+import android.widget.ImageButton;
 
-public class Luggage extends Fragment {
-
+public class Luggage extends Fragment implements View.OnClickListener {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.activity_luggage, container, false);
+        View myView = inflater.inflate(R.layout.activity_luggage, container, false);
+        ImageButton front4 = myView.findViewById(R.id.front4);
+        front4.setOnClickListener(this);
+        return myView;
+    }
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+    @Override
+    public void onClick(View v) {
+        Fragment fragment = new Early();
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+        ft.replace(R.id.screen_area, fragment, "Luggage");
+        ft.commit();
     }
 }

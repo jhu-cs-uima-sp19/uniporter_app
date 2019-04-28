96df1599a0aa3781c32b06760dca2202713432c7
Lilian Zhou <lilianhyz@gmail.com>
2019-04-28 18:01:16 -0400

    notifications

--- app/src/main/java/com/example/uniporter_app/MyFirebaseMessagingService.java
+++ app/src/main/java/com/example/uniporter_app/MyFirebaseMessagingService.java
@@ -36,7 +36,7 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         Uri soundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
         NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
                 .setSmallIcon(R.mipmap.ic_launcher)
-                .setContentTitle("C# Corner")
+                .setContentTitle("NOTIFICATION")
                 .setContentText(messageBody)
                 .setAutoCancel(true)
                 .setSound(soundUri)

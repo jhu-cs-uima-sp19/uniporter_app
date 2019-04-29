6aa30487791cef5e2e8323e78ac8f44d5a9b121b
mrajmoh1 <mrajmoh1@jhu.edu>
2019-04-28 23:26:29 -0400

    added spinners to profile but does not display name and email

--- app/src/main/java/com/example/uniporter_app/Profile.java
+++ app/src/main/java/com/example/uniporter_app/Profile.java
@@ -16,16 +16,18 @@ import com.example.uniporter_app.Storage.SharedPreferenceManager;
 public class Profile extends AppCompatActivity {
     String user_email;
     String user_name;
+    TextView email_text = (TextView) findViewById(R.id.email);
+    TextView name_text = (TextView) findViewById(R.id.name);
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile);
 
-        TextView name_text = (TextView) findViewById(R.id.name);
+
         user_name = SharedPreferenceManager.getInstance(Profile.this).getName();
         name_text.setText(user_name);
 
-        TextView email_text = (TextView) findViewById(R.id.email);
+
         user_email = SharedPreferenceManager.getInstance(Profile.this).getUserEmail();
         email_text.setText(user_email);
 

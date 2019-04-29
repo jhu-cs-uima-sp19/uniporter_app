307aa46bb39491a00606ead708fb746c14377b72
jxing8 <jxing8@jhu.edu>
2019-04-28 22:44:54 -0400

    fade animation

--- app/src/main/java/com/example/uniporter_app/AnimationUtil.java
+++ app/src/main/java/com/example/uniporter_app/AnimationUtil.java
@@ -1,35 +1,17 @@
 package com.example.uniporter_app;
 
-import android.animation.AnimatorSet;
-import android.animation.ObjectAnimator;
 import android.support.v7.widget.RecyclerView;
-import android.view.animation.Animation;
-import android.view.animation.ScaleAnimation;
+import android.view.View;
+import android.view.animation.AlphaAnimation;
 
 public class AnimationUtil {
-    private final static int FADE_DURATION = 1000; //FADE_DURATION in milliseconds
+    private final static int FADE_DURATION = 2000; //FADE_DURATION in milliseconds
 
-    public static void animate(RecyclerView.ViewHolder holder ,boolean goesDown){
-
-        ScaleAnimation anim = new ScaleAnimation(0.0f, 1.0f, 0.0f, 1.0f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+    public static void animate(RecyclerView.ViewHolder holder, boolean isScale){
+        View view = holder.itemView;
+        AlphaAnimation anim = new AlphaAnimation(0.0f, 1.0f);
         anim.setDuration(FADE_DURATION);
-        holder.itemView.startAnimation(anim);
-
-        /*
-        AnimatorSet animatorSet = new AnimatorSet();
-
-        ObjectAnimator animatorTranslateY = ObjectAnimator.ofFloat(holder.itemView, "translationY", goesDown==true ? 200 : -200, 0);
-        animatorTranslateY.setDuration(1000);
-
-
-        ObjectAnimator animatorTranslateX = ObjectAnimator.ofFloat(holder.itemView,"translationX",-50,50,-30,30,-20,20,-5,5,0);
-        animatorTranslateX.setDuration(1000);
-
-        animatorSet.playTogether(animatorTranslateX,animatorTranslateY);
-
-        //animatorSet.playTogether(animatorTranslateY);
-        animatorSet.start();
-        */
-
+        view.startAnimation(anim);
     }
 }
+

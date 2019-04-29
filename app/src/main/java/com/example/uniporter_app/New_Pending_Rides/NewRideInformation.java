0d357e7578d6e14b1339ade99412f602476bce1b
jxing8 <jxing8@jhu.edu>
2019-04-28 21:09:22 -0400

    Group rides by past and pending

--- app/src/main/java/com/example/uniporter_app/New_Pending_Rides/NewRideInformation.java
+++ app/src/main/java/com/example/uniporter_app/New_Pending_Rides/NewRideInformation.java
@@ -1,6 +1,10 @@
 package com.example.uniporter_app.New_Pending_Rides;
 
-public class NewRideInformation {
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class NewRideInformation implements Comparable< NewRideInformation >{
     public Integer id;
     public String type;
     public String airline;
@@ -9,6 +13,20 @@ public class NewRideInformation {
     public String flight_time;
 
     public int getViewType() {
-        return 1;
+        return 6500;
+    }
+
+    @Override
+    public int compareTo(NewRideInformation newRideInformation) {
+        SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yy");
+        Date this_date = null;
+        Date other_date = null;
+        try {
+            this_date = sdf.parse(this.date);
+            other_date = sdf.parse(newRideInformation.date);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return this_date.compareTo(other_date);
     }
 }

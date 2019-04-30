360077330cc94ff41a5363ed6dbcff112f9f4cff
Lilian Zhou <lilianhyz@gmail.com>
2019-04-30 18:06:35 -0400

    messages sorted

--- app/src/main/java/com/example/uniporter_app/Messenger.java
+++ app/src/main/java/com/example/uniporter_app/Messenger.java
@@ -6,6 +6,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.view.View;
+import java.util.Arrays;
+import java.util.Comparator;
+
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -25,7 +28,32 @@ public class Messenger extends AppCompatActivity {
        myDatabase.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                myTexts.setText(dataSnapshot.getValue().toString());
+                if(dataSnapshot.getValue().toString().equals("")) {
+                    return;
+                }
+                String[] Messages = dataSnapshot.getValue().toString().split(",");
+                myTexts.setText("");
+                String[][] finalMsgs = new String[Messages.length][2];
+                for(int i = 0; i < Messages.length; i++) {
+                    String[] finalMsg = Messages[i].split("=");
+                    finalMsgs[i][0] = finalMsg[0];
+                    finalMsgs[i][1] = finalMsg[1];
+                }
+               Arrays.sort(finalMsgs, new Comparator<String[]>() {
+                    @Override
+                    //arguments to this method represent the arrays to be sorted
+                    public int compare(String[] o1, String[] o2) {
+                        //get the item ids which are at index 0 of the array
+                        String itemIdOne = o1[0];
+                        String itemIdTwo = o2[0];
+                        // sort on item id
+                        return itemIdOne.compareTo(itemIdTwo);
+                    }
+                });
+                for(int i = 0; i < Messages.length; i++) {
+                    myTexts.append(finalMsgs[i][1] + "\n");
+                }
+
             }
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
@@ -35,8 +63,13 @@ public class Messenger extends AppCompatActivity {
     }
     public void sendMessage(View view) {
         final EditText myEdits = findViewById(R.id.messagetext);
-        myDatabase.push().setValue(myEdits.getText().toString());
+        String pending_message = myEdits.getText().toString();
+        if (myEdits.getText().toString().trim().equals("")) {
+            return;
+        }
+        myDatabase.child(Long.toString(System.currentTimeMillis())).setValue(pending_message);
         myEdits.setText("");
+
     }
 
 }
